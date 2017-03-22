cd971d0de02164a88123db9375dce0a47456d668
iPablo <pabloig90@gmail.com>
2017-03-22 17:33:47 +0100

    Fixed pep8 error

--- project_name/base/settings.py
+++ project_name/base/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         # 'DIRS': ['../templates'],
-	'APP_DIRS': True,
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
