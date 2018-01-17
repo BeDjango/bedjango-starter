cbda84b1d697a5792141eea5162877c6a46c5cbb
bereware <bereware@gmail.com>
2018-01-17 10:58:32 +0100

    Better code quality

--- project_name/base/static/js/login_validator.js
+++ project_name/base/static/js/login_validator.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    if($('#modal-login').has('.errorlist')){
-        $('#modal-login').modal('show');
+    if($("#modal-login").has(".errorlist")){
+        $("#modal-login").modal("show");
     }
 });
